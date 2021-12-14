3a6a9e5b6978e3cecb719a6cd5315dcfc7b4b8c1
holukas <lukas.hoertnagl@usys.ethz.ch>
2021-12-14 01:03:30 +0100

    Fixing imports 22

--- dataflow/modules/__init__.py
+++ dataflow/modules/__init__.py
@@ -1,1 +1,8 @@
-# from .logger import *
+from . import cli
+from . import clients
+from . import datascanner
+from . import filereader
+from . import filescanner
+from . import logger
+from . import pagebuilder
+from . import varscanner
